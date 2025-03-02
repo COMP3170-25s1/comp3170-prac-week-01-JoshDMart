9821d40e94972a6712e9756b397225792fad3c1b
JoshDMart <joshua.martin4@students.mq.edu.au>
2025-03-02 19:59:01 +1100

    Diamond Created.

--- src/comp3170/week1/fragment.glsl
+++ src/comp3170/week1/fragment.glsl
@@ -8,14 +8,16 @@ layout(location = 0) out vec4 o_colour;	// output to colour buffer
 void main() {
    vec2 p = gl_FragCoord.xy / u_screenSize;   // scale p into range (0,0) to (1,1)
    //float d = distance(p, vec2(0.5, 0.5));  // calculate distance to midpoint
+  float aspectRatio = u_screenSize.x / u_screenSize.y;
   
-  vec2 v = abs(p - vec2(0.5, 0.5));
-  float d = max(v.x, v.y);     
+  
+  vec2 v = abs(p - vec2(0.5, 0.5)); //used to make sure the difference from the middle to the point is measured without regard to direction (no negative distance)
+  float d = v.x * aspectRatio + v.y;   
   
    if (d < 0.3) {
       o_colour = vec4(u_colour, 1);
    }
    else {
-      o_colour = vec4(0,0,1,0); // BLACK
+      o_colour = vec4(0,0,0,1); // BLACK
    }
 }
